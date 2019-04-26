12240d9d3655cbd5197821d3e373b2751241c078
Simon Belanger <simon-2.belanger@polymtl.ca>
2019-04-26 16:08:00 -0400

    another log attempt

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import json
-import logging
+import sys
 from flask import Flask, request
 from serve import is_text_polite
 
@@ -8,8 +8,9 @@ app = Flask(__name__)
 @app.route('/evaluate', methods=['POST'])
 def evaluate():
     input_data = request.json
-    logging.debug('request JSON %s', input_data)
-    text_to_evaluate = input_data[0]['text']
+    print(input_data)
+    sys.stdout.flush()
+    text_to_evaluate = input_data['text']
 
     output_data = {
         "is_polite":is_text_polite(text_to_evaluate)
