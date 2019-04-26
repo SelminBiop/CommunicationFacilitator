303d6100047a9a30ca1124c92cb5073efe87c868
Simon Belanger <simon-2.belanger@polymtl.ca>
2019-04-26 16:40:52 -0400

    no tkenization for now since unable to download it in heroku

--- serve.py
+++ serve.py
@@ -1,11 +1,11 @@
 import pandas as pd
-import nltk
 from nltk.corpus import stopwords
 
 negative_words_path = "data/negative_words_fr.csv"
 
 def is_text_polite(text):
-    tokens = nltk.word_tokenize(text, language='french')
+    #tokens = nltk.word_tokenize(text, language='french')
+    tokens = text.split(" ")
     useful_tokens = [token for token in tokens if token not in stopwords.words('french')]
     neg_words = pd.read_csv(negative_words_path)
     for token in useful_tokens:
