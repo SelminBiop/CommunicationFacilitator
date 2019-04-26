3f3d75144abb44d539f939c54a2523e021d0e81b
Simon Belanger <simon-2.belanger@polymtl.ca>
2019-04-26 15:52:18 -0400

    Added french sentiment lexicon and json parsing

--- script.py
+++ script.py
@@ -7,9 +7,10 @@ app = Flask(__name__)
 @app.route('/evaluate', methods=['POST'])
 def evaluate():
     input_data = request.json
+    text_to_evaluate = input_data[0]['text']
 
     output_data = {
-        "is_polite":is_text_polite("")
+        "is_polite":is_text_polite(text_to_evaluate)
     }
     response = json.dumps(output_data)
 
