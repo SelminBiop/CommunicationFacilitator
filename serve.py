6928df3781b0f69d22b5afbefeeac50afb1bc9ce
Simon Belanger <simon-2.belanger@polymtl.ca>
2019-04-26 16:36:08 -0400

    changed how words are transformed into tokens

--- serve.py
+++ serve.py
@@ -1,11 +1,11 @@
 import pandas as pd
+import nltk
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 
 negative_words_path = "data/negative_words_fr.csv"
 
 def is_text_polite(text):
-    tokens = word_tokenize(text, language='french')
+    tokens = nltk.word_tokenize(text, language='french')
     useful_tokens = [token for token in tokens if token not in stopwords.words('french')]
     neg_words = pd.read_csv(negative_words_path)
     for token in useful_tokens:
