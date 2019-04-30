adf66ab8b8a7baf75d82c970310d5c238ceed146
Simon Belanger <simon-2.belanger@polymtl.ca>
2019-04-30 12:01:26 -0400

    trying to figure out if its json dump that is broken or if it is received text

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import json
+import sys
 from flask import Flask, request
 from serve import is_text_polite
 
@@ -11,6 +12,8 @@ def evaluate():
     text_to_evaluate = input_data["text"]
 
     corrected_text, is_polite = is_text_polite(text_to_evaluate)
+    print(text_to_evaluate)
+    sys.stdout.flush()
 
     output_data = {
         "is_polite":is_polite,
