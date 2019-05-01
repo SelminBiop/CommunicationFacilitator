49129c1b7d9b3b837d9e8e3bc0a23c9127ab6126
Simon Belanger <simon-2.belanger@polymtl.ca>
2019-05-01 09:57:28 -0400

    model loading trying with inherited Flask class

--- script.py
+++ script.py
@@ -1,21 +1,34 @@
 import json
-import nlputils
+import fr_core_news_sm
+from symspellpy.symspellpy import SymSpell
 from flask import Flask, request
 from serve import is_text_polite
 
-fr_dict_path = "data/fr_full.txt"
+class FlaskApp(Flask):
 
-app = Flask(__name__)
+    def __init__(self, *args, **kwargs):
+        super(FlaskApp, self).__init__(*args, **kwargs)
+        self.spacy_model = fr_core_news_sm.load()
+
+        max_edit_distance_dictionary = 2
+        prefix_length = 7
+        self.sym_spell = SymSpell(max_edit_distance_dictionary, prefix_length)
+        
+        term_index = 0
+        count_index = 1
+        
+        self.sym_spell.load_dictionary("data/fr_full.txt", term_index, count_index)
+
+
+app = FlaskApp(__name__)
 app.config['DEBUG'] = True
-
-utils = NLPutils(fr_dict_path)
 
 @app.route('/evaluate', methods=['POST'])
 def evaluate():
     input_data = request.json
     text_to_evaluate = input_data["text"]
 
-    corrected_text, is_polite = is_text_polite(text_to_evaluate, utils)
+    corrected_text, is_polite = is_text_polite(text_to_evaluate)
 
     output_data = {
         "is_polite":is_polite,
--- serve.py
+++ serve.py
@@ -1,11 +1,11 @@
 import pandas as pd
-import nlputils
 from symspellpy.symspellpy import Verbosity
+from flask import current_app as app
 
 negative_words_path = "data/negative_words_fr.csv"
 
-def is_text_polite(text, nlputils):
-    tokens = nlputils.spacy_model(text)
+def is_text_polite(text):
+    tokens = app.spacy_model(text)
     #useful_tokens = [token for token in tokens if token not in stopwords.words('french')]
 
     #neg_words = pd.read_csv(negative_words_path)
@@ -17,7 +17,7 @@ def is_text_polite(text, nlputils):
 
     for token in tokens:
         word = token.text.lower()
-        suggestions = nlputils.sym_spell.lookup(word, suggestion_verbosity, max_edit_distance_lookup)
+        suggestions = app.sym_spell.lookup(word, suggestion_verbosity, max_edit_distance_lookup)
         corrected_tokens.append(word if len(suggestions) < 1 else suggestions[0].term)
         #if word in neg_words['Word'].values:
             #polite = False
