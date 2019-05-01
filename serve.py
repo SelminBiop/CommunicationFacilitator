536c4620896154d49af656e77d6130badbca21d6
Simon Belanger <simon-2.belanger@polymtl.ca>
2019-05-01 12:29:02 -0400

    trying other symspell params

--- serve.py
+++ serve.py
@@ -7,10 +7,9 @@ fr_dict_path = "data/fr_full.txt"
 
 #sp = fr_core_news_sm.load()
 
-max_edit_distance_dictionary = 2
-prefix_length = 7
+max_edit_distance_dictionary = 1
 
-sym_spell = SymSpell(max_edit_distance_dictionary, prefix_length)
+sym_spell = SymSpell(max_edit_distance_dictionary)
 
 term_index = 0
 count_index = 1
@@ -18,7 +17,7 @@ count_index = 1
 sym_spell.load_dictionary(fr_dict_path, term_index, count_index)
 
 max_edit_distance_lookup = 2
-suggestion_verbosity = Verbosity.CLOSEST 
+suggestion_verbosity = Verbosity.TOP 
 
 def is_text_polite(text):    
     tokens = text.split(" ")
