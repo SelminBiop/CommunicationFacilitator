76397b54f4700c37bea1b416fcaec779ebccc861
Simon Belanger <simon-2.belanger@polymtl.ca>
2019-05-01 10:33:34 -0400

    trying to resolve memory error

--- script.py
+++ script.py
@@ -1,26 +1,8 @@
 import json
-import fr_core_news_sm
-from symspellpy.symspellpy import SymSpell
 from flask import Flask, request
 from serve import is_text_polite
 
-class FlaskApp(Flask):
-
-    def __init__(self, *args, **kwargs):
-        super(FlaskApp, self).__init__(*args, **kwargs)
-        self.spacy_model = fr_core_news_sm.load()
-
-        #max_edit_distance_dictionary = 2
-        #prefix_length = 7
-        #self.sym_spell = SymSpell(max_edit_distance_dictionary, prefix_length)
-        
-        #term_index = 0
-        #count_index = 1
-        
-        #self.sym_spell.load_dictionary("data/fr_full.txt", term_index, count_index)
-
-
-app = FlaskApp(__name__)
+app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route('/evaluate', methods=['POST'])
--- serve.py
+++ serve.py
@@ -1,24 +1,37 @@
 import pandas as pd
-from symspellpy.symspellpy import Verbosity
-from flask import current_app as app
+import fr_core_news_sm
+#from symspellpy.symspellpy import SymSpell, Verbosity
 
 negative_words_path = "data/negative_words_fr.csv"
+fr_dict_path = "data/fr_full.txt"
 
-def is_text_polite(text):
-    tokens = app.spacy_model(text)
+sp = fr_core_news_sm.load()
+
+def is_text_polite(text):    
+    tokens = sp(text)
     #useful_tokens = [token for token in tokens if token not in stopwords.words('french')]
 
     #neg_words = pd.read_csv(negative_words_path)
     polite = True
     corrected_tokens = []
+    
+    max_edit_distance_dictionary = 2
+    prefix_length = 7
+    
+    #sym_spell = SymSpell(max_edit_distance_dictionary, prefix_length)
+
+    term_index = 0
+    count_index = 1
+
+    #sym_spell.load_dictionary(fr_dict_path, term_index, count_index)
 
     max_edit_distance_lookup = 2
-    suggestion_verbosity = Verbosity.CLOSEST    
+    #suggestion_verbosity = Verbosity.CLOSEST    
 
     for token in tokens:
         word = token.text.lower()
-        #suggestions = app.sym_spell.lookup(word, suggestion_verbosity, max_edit_distance_lookup)
-        corrected_tokens.append(word) #if len(suggestions) < 1 else suggestions[0].term)
+        #suggestions = sym_spell.lookup(word, suggestion_verbosity, max_edit_distance_lookup)
+        corrected_tokens.append(word)# if len(suggestions) < 1 else suggestions[0].term)
         #if word in neg_words['Word'].values:
             #polite = False
     return corrected_tokens, polite
