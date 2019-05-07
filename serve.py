7bc387030fb2e4134e3de406d5c03b0310e374d1
Simon Belanger <simon-2.belanger@polymtl.ca>
2019-05-07 18:13:50 -0400

    Trying to specify encoding, changed how politeness is determined

--- serve.py
+++ serve.py
@@ -10,17 +10,21 @@ db_url = os.environ['DATABASE_URL']
 def is_text_polite(text):    
 
     document = types.Document(content=text, type=enums.Document.Type.PLAIN_TEXT)
-    annotations = client.analyze_sentiment(document=document)
+    annotations = client.analyze_sentiment(document=document, encodingType=enums.EncodingType.UTF32)
 
     score = annotations.document_sentiment.score
     magnitude = annotations.document_sentiment.magnitude
 
+    sum_score = 0
+
     for index, sentence in enumerate(annotations.sentences):
         sentence_sentiment = sentence.sentiment.score
-        sys.stdout.write('Sentence {} has a sentiment score of {}'.format(sentence.text, sentence_sentiment))
+        sentence_text = sentence.text.content
+        sum_score += sentence_sentiment
+        sys.stdout.write('Sentence {} has a sentiment score of {}'.format(sentence_text, sentence_sentiment))
         sys.stdout.flush()
     
     sys.stdout.write('Overall Sentiment: score of {} with magnitude of {}'.format(score, magnitude))
     sys.stdout.flush()
         
-    return text, score >= 0
+    return text, sum_score >= 0
