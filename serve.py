4c7b3678c0a6a181e0a9d72cc23c6b7c2a5c0a5a
Simon Belanger <simon-2.belanger@polymtl.ca>
2019-05-07 14:38:07 -0400

    Gathering data using google natural language API as initial sentiment analysis

--- serve.py
+++ serve.py
@@ -1,11 +1,3 @@
-import fr_core_news_sm
-import sys
-from pattern.fr import sentiment
-
-negative_words_path = "data/negative_words_fr.csv"
-
-sp = fr_core_news_sm.load()
-
 def is_text_polite(text):
     tokens = sp(text)
     useful_tokens = [token for token in tokens if not token.is_stop]
@@ -19,12 +11,6 @@ def is_text_polite(text):
 
     pol_sum = 0
     for sentence in sentences:
-        polarity = sentiment(sentence.text)[0]
-        subjectivity = sentiment(sentence.text)[1]
         pol_sum += polarity
-        sys.stdout.write(sentence.text + '\n')
-        sys.stdout.write('Polarity : ' + str(polarity) + '\n')
-        sys.stdout.write('Subjectivity : ' + str(subjectivity) + '\n')
-        sys.stdout.flush()
         
     return corrected_tokens, pol_sum >= 0
